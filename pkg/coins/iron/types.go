69415081d78b1e43cd8fb013ca45d1997479454f
coast <670884108@qq.com>
2023-03-12 21:32:27 +0800

    add accont struct

--- pkg/coins/iron/types.go
+++ pkg/coins/iron/types.go
@@ -4,6 +4,13 @@ import (
 	ct "github.com/NpoolPlatform/sphinx-plugin-p3/pkg/types"
 )
 
+type IronfishAccount struct {
+	PublicKey   string `json:"publicKey"`
+	ViewKey     string `json:"viewKey"`
+	OutgoingKey string `json:"outgoingKey"`
+	IncomingKey string `json:"incomingKey"`
+}
+
 type SignMsgTx struct {
 	BaseInfo        ct.BaseInfo `json:"base_info"`
 	RecentBlockHash string      `json:"recent_block_hash"`
