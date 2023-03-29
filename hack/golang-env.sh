9c7557ae6f6e1642f6d35f9011426bc5e21ac093
coast <670884108@qq.com>
2023-03-29 15:57:12 +0800

    using https_proxy

--- hack/golang-env.sh
+++ hack/golang-env.sh
@@ -14,7 +14,7 @@ echo "Will change go version to $go_name"
 
 go_tar="go1.19.6.linux-amd64.tar.gz"
 go_tar_url="https://go.dev/dl/$go_tar"
-go_data=/tmp/$go_name
+go_data=~/.golang/$go_name
 go_path=$go_data/gopath
 go_root=$go_data/goroot
 go_env_file=$go_data/goenv.sh
@@ -30,14 +30,15 @@ export GOPROXY="https://proxy.golang.org,direct"
 shopt -s expand_aliases
 alias go="$go_root/bin/go"
 
-set +e
-rc=`go version | grep $go_name`
-if [ ! $? -eq 0 ]; then
+# set +e
+# rc=`go version | grep $go_name`
+# if [ ! $? -eq 0 ]; then
   echo "Fetching $go_tar from $go_tar_url, stored to $go_data"
-  curl -sL $go_tar_url -o $go_data/$go_tar
+  curl -L $go_tar_url -o $go_data/$go_tar
+  md5sum $go_data/$go_tar
   tar -zxvf $go_data/$go_tar --strip-components 1 -C $go_root
-fi
-set -e
+# fi
+# set -e
 
 
 
