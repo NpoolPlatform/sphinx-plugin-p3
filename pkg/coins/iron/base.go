0443b41816d766a4988cf9ed4a4b0f04bf705466
Geapefurit <670884108@qq.com>
2023-06-05 19:28:03 +0800

    replace assertid to chainid

--- pkg/coins/iron/base.go
+++ pkg/coins/iron/base.go
@@ -24,7 +24,7 @@ const (
 	ChainNativeUnit     = "IRON"
 	ChainAtomicUnit     = "ORE"
 	ChainUnitExp        = 8
-	ChainID             = "1"
+	ChainID             = "51f33a2f14f92735e562dc658a5639279ddca3d5079a6d1242b2a588a9cbf44c"
 	ChainNickname       = "Ironfish"
 	ChainNativeCoinName = "ironfish"
 )
